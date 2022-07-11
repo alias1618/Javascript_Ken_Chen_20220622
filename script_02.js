d4760696453e6dd87c950bb8faac84c5533c6987
DESKTOP-BKTE25U\home <wei.liang.hong84@gmail.com>
2022-07-12 06:40:17 +0800

    20220712_function 創建階段, function 運行階段

--- script_02.js
+++ script_02.js
@@ -64,3 +64,67 @@ HelloKen
 */
 
 
+//===================Vo-Variable Object==================================
+
+//Execution context
+
+// 1. 創建
+
+//     初始化空間 - Scope chain;
+//     創建 VO - Variable Object;
+
+//     function(arguemts object)
+//     掃描function - PointerEvent
+//     掃描variable - underfined
+
+//     this Variable
+
+// 2. 運行
+
+//     進行每一行的運行
+
+// function foo(i){
+//     var a = "Hello";
+//     var b = function privateB(){
+
+//     };
+//     function c(){
+
+//     }
+// }
+
+// foo(22);
+
+// //function 創建階段
+// fooExecutionContext = {
+//     scopeChain: {...},
+//     variableObject: {
+//         arguemts:{
+//             0:22,
+//             length:1
+//         }
+//         i: 22,
+//         c: pointer to function c()
+//         b: underfined,
+//         a: underfined,
+//     }
+// }
+
+// this: {...}
+
+// //function 運行階段
+// fooExecutionContext = {
+//     scopeChain: {...},
+//     variableObject: {
+//         0:22,
+//         length: 1
+    
+//     }
+//     i: 22,
+//     c: pointer to function c()
+//     a: 'Hello',
+//     b: pointer to function privateB()
+
+// }
+
+// this: {...}
