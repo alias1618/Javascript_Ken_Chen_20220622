5cb177ffb204a4f6e7d30876e113180724902bc9
DESKTOP-BKTE25U\home <wei.liang.hong84@gmail.com>
2022-07-26 07:47:13 +0800

    20220726_4-33 搖骰子遊戲DOM

--- Game-JS/game.js
+++ Game-JS/game.js
@@ -3,3 +3,18 @@
 	遊戲JavaScript文件
 	
 */
+
+//骰子的數字
+
+var dice = Math.floor(Math.random() * 6) + 1;
+
+console.log(dice);
+
+var currentPlayer = 2;
+
+//DOM - Document Object Model
+
+//將隨機數字帶入score
+//document.querySelector('#player1-current-score').textContent = dice;
+
+document.querySelector('#player' + currentPlayer + '-current-score').textContent = dice;
