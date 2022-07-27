4b5c4af7d22388a4e5dc236ce212afa6dbab6122
DESKTOP-BKTE25U\home <wei.liang.hong84@gmail.com>
2022-07-28 07:24:41 +0800

    202220728_js如何修改HTML和css

--- Game-JS/game.js
+++ Game-JS/game.js
@@ -10,11 +10,21 @@ var dice = Math.floor(Math.random() * 6) + 1;
 
 console.log(dice);
 
-var currentPlayer = 2;
+var currentPlayer = 1;
 
 //DOM - Document Object Model
 
 //將隨機數字帶入score
 //document.querySelector('#player1-current-score').textContent = dice;
 
-document.querySelector('#player' + currentPlayer + '-current-score').textContent = dice;
+//document.querySelector('#player' + currentPlayer + '-current-score').textContent = dice;
+
+document.querySelector('#player' + currentPlayer + '-current-score').innerHTML ='<h1>'+ dice +'<h1>';
+
+//隱藏骰子
+document.querySelector('.dice').style = 'display: none';
+
+//隱藏獎盃
+document.querySelector('.winner1').style = 'display: none';
+
+document.querySelector('.winner2').style = 'display: none';
