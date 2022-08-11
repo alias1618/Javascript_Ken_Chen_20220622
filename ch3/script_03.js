e0fdae926cabad3a8428f7a529fb82ad6f7219c0
DESKTOP-BKTE25U\home <wei.liang.hong84@gmail.com>
2022-08-12 06:32:03 +0800

    20220810_update

--- ch3/script_03.js
+++ ch3/script_03.js
@@ -1,22 +1,44 @@
 //=====================Primitive and Object============================
 
-var a = 22;
-var b = a;
+// var a = 22;
+// var b = a;
 
-a = 35;
+// a = 35;
 
-console.log(a);
-console.log(b);
+// console.log(a);
+// console.log(b);
 
-var c = {
+// var c = {
 
-    name: 'Ken',
+//     name: 'Ken',
+// }
+// console.log(c.name);
+// var d = c;
+// console.log(d);
+
+// c.name = 'Tom';
+
+// console.log(c.name);
+// console.log(d.name);
+
+
+
+var age = 26;
+
+var obj = {
+
+    name: 'Tommy',
+    lastName: 'Li',
+
 }
-console.log(c.name);
-var d = c;
-console.log(d);
 
-c.name = 'Tom';
+function change(a, b) {
 
-console.log(c.name);
-console.log(d.name);
+    a = 36;
+    b.lastName = 'Wang';
+}
+
+change (age, obj);
+
+console.log(age); //26
+console.log(obj.lastName); //lastName: 'Wang',
