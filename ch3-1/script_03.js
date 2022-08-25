cec4e7e753bc420a5422a2192ad1b2b8fe61c961
DESKTOP-BKTE25U\home <wei.liang.hong84@gmail.com>
2022-08-26 06:48:51 +0800

    20220826_IIFE - immediately invoked function expressions

--- ch3-1/script_03.js
+++ ch3-1/script_03.js
@@ -1,30 +1,93 @@
+//================如何使用function作為function的參數==========================
+// var years = [1993, 1956, 1982, 1933]
 
-var years = [1993, 1956, 1982, 1933]
+// function calculate(arr, func){
 
-function calculate(arr, func){
+//     var array = [];
+//     for(var i = 0; i <arr.length; i++){
 
-    var array = [];
-    for(var i = 0; i <arr.length; i++){
+//         array.push(func(arr[i]));
+//     }
+//     return array;
+// }
 
-        array.push(func(arr[i]));
-    }
-    return array;
-}
+// function ageCalculate(arr){
 
-function ageCalculate(arr){
+//     return 2017 - arr;
+// }
 
-    return 2017 - arr;
-}
+// function isFullAges(arr){
 
-function isFullAges(arr){
+//     return arr >= 18;
+// }
 
-    return arr >= 18;
-}
+// var ages = calculate(years, ageCalculate);
 
-var ages = calculate(years, ageCalculate);
+// console.log(ages);
 
-console.log(ages);
+// var fullAges = calculate(ages, isFullAges);
 
-var fullAges = calculate(ages, isFullAges);
+// console.log(fullAges);
 
-console.log(fullAges);
+
+
+//======================================================================
+
+
+// function sayHello(job) {
+
+//     return function(name){
+
+//         if(job === 'designer') {
+
+//             console.log('Hello ' + name +', you are a designer!');
+//         }else if(job === 'teacher') {
+
+//             console.log('Hello '+ name +', you are a teacher!');
+//         }
+
+//     }
+// }
+
+// //var callFunc = sayHello('teacher');
+
+// //var callFunc = sayHello('designer');
+
+// //callFunc('Tommy');
+
+
+// sayHello('designer')('Daniel');
+
+
+
+//===========================IIFE - immediately invoked function expressions ======================
+
+// function sayHello() {
+
+//     console.log('Hello');
+
+// }
+
+// sayHello();
+
+
+//IIFE - immediately invoked function expressions
+
+// (function sayHello() {
+
+//     console.log('Hello, daniel');
+
+// })();
+
+var a = 2;
+
+
+//帶參數
+(function sayHello(name) {
+
+    console.log('Hello, '+ name +'!');
+
+})('Tommy');
+
+
+console.log(name);
