f20c52acc037a55ab51818350fdbd1f805f8cb50
DESKTOP-BKTE25U\home <wei.liang.hong84@gmail.com>
2022-09-21 07:54:38 +0800

    20220921_MVC之間數據的傳遞

--- HowManyThingsYouBought_static/scripts/app.js
+++ HowManyThingsYouBought_static/scripts/app.js
@@ -3,12 +3,40 @@
 
 var model = (function(){
 
-});
+})();
 
 var view = (function() {
 
-});
+    var DOMstrings = {
+        name: '.name',
+        value: '.value',
+        btn:'.bought_btn',
+    }
 
-var controller = (function (m,v) {
+    return {
+        getInfo: function(){
+            return{
+                name: document.querySelector(DOMstrings.name).value,
+                value: document.querySelector(DOMstrings.value).value,
+            };
+        },
+
+        getDOMstrings: function(){
+            return DOMstrings;
+        },
+    };
+})();
+
+var controller = (function (m, v) {
+
+    var DOMstrings = view.getDOMstrings();
+
+    var addItem = function() {
+
+        var input = view.getInfo();
+        console.log(input);
+    };
+
+    document.querySelector(DOMstrings.btn).addEventListener('click', addItem);
     
 })(model, view);
